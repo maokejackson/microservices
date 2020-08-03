89ebcc80c104b2e300da3210d6949c6ef1d86926
Maoke Jackson <maokejackson@gmail.com>
2020-08-04 00:15:42 +0800

    enable oauth resource server for review-service

---
+++ resource/review/src/main/java/com/dtxmaker/microservice/resource/review/SecurityConfig.java
@@ -0,0 +1,57 @@
+package com.dtxmaker.microservice.resource.review;
+
+import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter
+{
+    @Override
+    protected void configure(HttpSecurity http) throws Exception
+    {
+        // @formatter:off
+        http.cors()
+                .and()
+            .csrf()
+                .disable()
+            .httpBasic()
+                .disable()
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+            .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+            .oauth2ResourceServer()
+                .jwt()
+        ;
+        // @formatter:on
+    }
+
+    private JwtAuthenticationConverter jwtAuthenticationConverter()
+    {
+        final JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new KeycloakRealmRoleConverter());
+        return jwtAuthenticationConverter;
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoderByIssuerUri(OAuth2ResourceServerProperties properties)
+    {
+        final String jwkSetUri = properties.getJwt().getJwkSetUri();
+        final NimbusJwtDecoder jwtDecoder = NimbusJwtDecoder.withJwkSetUri(jwkSetUri).build();
+        // Use preferred_username from claims as authentication name, instead of UUID subject
+        jwtDecoder.setClaimSetConverter(new UsernameSubClaimAdapter());
+        return jwtDecoder;
+    }
+}
