f5d402553c60dca7a072e2df746984dad8805809
scw2 <Jackson.CHEW@3ds.com>
2020-08-05 18:22:34 +0800

    get roles from resource_access instead of realm_access

--- resource/movie/src/main/java/com/dtxmaker/microservice/resource/movie/config/KeycloakRealmRoleConverter.java
+++ resource/movie/src/main/java/com/dtxmaker/microservice/resource/movie/config/KeycloakRealmRoleConverter.java
@@ -12,16 +12,19 @@ import java.util.stream.Collectors;
 
 public class KeycloakRealmRoleConverter implements Converter<Jwt, Collection<GrantedAuthority>>
 {
-    public static final String REALM_ACCESS = "realm_access";
-    public static final String ROLES        = "roles";
-    public static final String ROLE_PREFIX  = "ROLE_";
+    public static final String RESOURCE_ACCESS = "resource_access";
+    public static final String RESOURCE_ID     = "movie-service";
+    public static final String ROLES           = "roles";
+    public static final String ROLE_PREFIX     = "ROLE_";
 
     @SuppressWarnings("unchecked")
     @Override
     public Collection<GrantedAuthority> convert(final Jwt jwt)
     {
-        final Map<String, Object> realmAccess = (Map<String, Object>) jwt.getClaims().get(REALM_ACCESS);
-        return ((List<String>) realmAccess.get(ROLES)).stream()
+        final Map<String, Map<String, List<String>>> resourceAccess = (Map<String, Map<String, List<String>>>) jwt
+                .getClaims().get(RESOURCE_ACCESS);
+
+        return resourceAccess.get(RESOURCE_ID).get(ROLES).stream()
                 .map(roleName -> ROLE_PREFIX + roleName)
                 .map(SimpleGrantedAuthority::new)
                 .collect(Collectors.toList());
