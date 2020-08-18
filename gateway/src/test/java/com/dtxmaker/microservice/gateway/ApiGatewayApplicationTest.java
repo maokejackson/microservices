f77f4883ac169226011303904015a51efeaea683
Maoke Jackson <maokejackson@gmail.com>
2020-08-18 22:50:17 +0800

    add test

---
+++ gateway/src/test/java/com/dtxmaker/microservice/gateway/ApiGatewayApplicationTest.java
@@ -0,0 +1,14 @@
+package com.dtxmaker.microservice.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTest
+{
+    @Test
+    void loadContext()
+    {
+        
+    }
+}
