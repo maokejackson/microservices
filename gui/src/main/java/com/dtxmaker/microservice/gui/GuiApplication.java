ba5cebe1ac1eea5cef55efd3d886693fa3a90836
scw2 <Jackson.CHEW@3ds.com>
2020-08-19 09:59:55 +0800

    enable eureka client for gui

--- gui/src/main/java/com/dtxmaker/microservice/gui/GuiApplication.java
+++ gui/src/main/java/com/dtxmaker/microservice/gui/GuiApplication.java
@@ -2,7 +2,9 @@ package com.dtxmaker.microservice.gui;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class GuiApplication
 {
