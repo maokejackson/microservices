71333ac5fbba2f2218d936ac83320501da324609
scw2 <Jackson.CHEW@3ds.com>
2020-08-19 10:57:57 +0800

    use DiscoveryClient to resolve api-gateway url

--- gui/src/main/java/com/dtxmaker/microservice/gui/config/RestTemplateConfig.java
+++ gui/src/main/java/com/dtxmaker/microservice/gui/config/RestTemplateConfig.java
@@ -2,22 +2,42 @@ package com.dtxmaker.microservice.gui.config;
 
 import org.keycloak.adapters.springsecurity.client.KeycloakClientRequestFactory;
 import org.keycloak.adapters.springsecurity.client.KeycloakRestTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 import org.springframework.web.util.DefaultUriBuilderFactory;
 
+import java.util.List;
+
 @Configuration
 public class RestTemplateConfig
 {
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
     @Bean
     @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
     public KeycloakRestTemplate keycloakRestTemplate(KeycloakClientRequestFactory keycloakClientRequestFactory)
     {
-        KeycloakRestTemplate restTemplate = new KeycloakRestTemplate(keycloakClientRequestFactory);
-        restTemplate.setUriTemplateHandler(new DefaultUriBuilderFactory("http://localhost:8084"));
-        return restTemplate;
+        List<ServiceInstance> instances = discoveryClient.getInstances("api-gateway");
+
+        if (instances.isEmpty())
+        {
+            return new KeycloakRestTemplate(keycloakClientRequestFactory);
+        }
+        else
+        {
+            ServiceInstance instance = instances.get(0);
+            String baseUri = instance.getUri().toString();
+
+            KeycloakRestTemplate restTemplate = new KeycloakRestTemplate(keycloakClientRequestFactory);
+            restTemplate.setUriTemplateHandler(new DefaultUriBuilderFactory(baseUri));
+            return restTemplate;
+        }
     }
 
     @Bean
