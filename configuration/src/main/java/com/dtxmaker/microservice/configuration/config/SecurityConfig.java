ca54cba192dcf90c243400d10443c48f9fa7870c
Maoke Jackson <maokejackson@gmail.com>
2020-10-02 21:53:36 +0800

    - declare user in properties instead of code - rename some properties name

--- configuration/src/main/java/com/dtxmaker/microservice/configuration/config/SecurityConfig.java
+++ configuration/src/main/java/com/dtxmaker/microservice/configuration/config/SecurityConfig.java
@@ -2,26 +2,14 @@ package com.dtxmaker.microservice.configuration.config;
 
 import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter
 {
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception
-    {
-        auth.inMemoryAuthentication()
-                .passwordEncoder(PasswordEncoderFactories.createDelegatingPasswordEncoder())
-                .withUser("config")
-                .password("{noop}config")
-                .roles("SYSTEM");
-    }
-
     @Override
     protected void configure(HttpSecurity http) throws Exception
     {
