ae36570fd35cb7cdafc8ff23b160a1d9a28134d1
Maoke Jackson <maokejackson@gmail.com>
2020-10-14 20:51:19 +0800

    fix gui unit test

--- gui/src/test/java/com/dtxmaker/microservice/gui/GuiApplicationTest.java
+++ gui/src/test/java/com/dtxmaker/microservice/gui/GuiApplicationTest.java
@@ -3,12 +3,12 @@ package com.dtxmaker.microservice.gui;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class GuiApplicationTest
 {
     @Test
     void loadContext()
     {
-        
+
     }
 }
